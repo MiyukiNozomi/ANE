b5752e35eed6ef3aa7975a55cc0ede71560b5b52
miyuki <miyuki@ane.jp.net>
2025-07-16 03:42:32 +0900

    Just an indentation change

--- ProductionUpdate.sh
+++ ProductionUpdate.sh
@@ -8,12 +8,13 @@ rm -rf /jails/containers/Galatea/SERVICE
 rm -rf /jails/containers/ReverseProxy/SERVICE
 
 echo "Updating SERVICES.."
-cp -r /root/ANE/GalateaCDN /jails/containers/Galatea/SERVICE
+cp -r /root/ANE/GalateaCDN   /jails/containers/Galatea/SERVICE
 cp -r /root/ANE/ReverseProxy /jails/containers/ReverseProxy/SERVICE
 
 echo "Restarting jails and running setup scripts..."
 service jail start galatea reverse-proxy
 
-jexec -u root galatea /SERVICE/setup
+jexec -u root galatea       /SERVICE/setup
 jexec -u root reverse-proxy /SERVICE/setup
+
 echo "Production is ready!"
