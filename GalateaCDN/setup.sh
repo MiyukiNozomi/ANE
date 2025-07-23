c22dfaa371669be25c6ee1b3dbaa23f11ce54d75
miyuki <miyuki@ane.jp.net>
2025-07-24 05:19:46 +0900

    Bad command..

--- GalateaCDN/setup.sh
+++ GalateaCDN/setup.sh
@@ -6,6 +6,6 @@ echo "Galatea uses PNPM! ensure it is installed beforehand!"
 
 pnpm i
 tsc
-su -m AZKi -c 'node prebuild.js'
+su - AZKi -c 'node prebuild.js'
 
 echo "No errors above? good, all done!"
