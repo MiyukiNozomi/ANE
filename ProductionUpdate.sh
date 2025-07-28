9094672d19b2e83b22ed5af805001fbf59a4b6d9
miyuki <miyuki@ane.jp.net>
2025-07-28 18:34:45 +0900

    Added AuthDaemon to Production

--- ProductionUpdate.sh
+++ ProductionUpdate.sh
@@ -3,21 +3,25 @@
 echo "Stopping all microservices (aka:"
 echo "Forbiding jails from running previous services)..."
 jexec -u root galatea       service jailservice stop 
-jexec -u root reverse-proxy service jailservice stop
+jexec -u root reverse-proxy service jailservice stop 
+jexec -u root auth-daemon service jailservice stop
 
 echo "Removing previous services..."
 rm -rf /jails/containers/Galatea/SERVICE
 rm -rf /jails/containers/ReverseProxy/SERVICE
+rm -rf /jails/containers/AuthDaemon/SERVICE
 
 echo "Updating SERVICES.."
 cp -r /root/ANE/GalateaCDN   /jails/containers/Galatea/SERVICE
 cp -r /root/ANE/ReverseProxy /jails/containers/ReverseProxy/SERVICE
+cp -r /root/ANE/AuthDaemon   /jails/containers/AuthDaemon/SERVICE
 
 # echo "Restarting jails and running setup scripts..."
 # service jail start galatea reverse-proxy
 
 jexec -u root galatea       /SERVICE/setup.sh
 jexec -u root reverse-proxy /SERVICE/setup.sh
+jexec -u root auth-daemon   /SERVICE/setup.sh
 
 echo "Production is ready!"
 echo "Do manual configuration now before restarting those services."
