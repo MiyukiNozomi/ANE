6a3eeecb870458b335f68753c0fa57d9e63fdc7e
miyuki <miyuki@ane.jp.net>
2025-07-30 07:56:06 +0900

    You know what? fuck it

--- ProductionUpdate.sh
+++ ProductionUpdate.sh
@@ -13,20 +13,21 @@ echo "Removing previous services..."
 rm -rf /jails/containers/Galatea/SERVICE
 rm -rf /jails/containers/ReverseProxy/SERVICE
 rm -rf /jails/containers/AuthDaemon/SERVICE
+rm -rf /jails/containers/Authentication/SERVICE
 
 echo "Updating SERVICES.."
 
-cp -r /root/ANE/GalateaCDN   /jails/containers/Galatea/SERVICE
-cp -r /root/ANE/ReverseProxy /jails/containers/ReverseProxy/SERVICE
-cp -r /root/ANE/AuthDaemon   /jails/containers/AuthDaemon/SERVICE
-cp -r /root/ANE/Authentication   /jails/containers/Authentication/SERVICE
+cp -r /root/ANE/GalateaCDN     /jails/containers/Galatea/SERVICE
+cp -r /root/ANE/ReverseProxy   /jails/containers/ReverseProxy/SERVICE
+cp -r /root/ANE/AuthDaemon     /jails/containers/AuthDaemon/SERVICE
+cp -r /root/ANE/Authentication /jails/containers/Authentication/SERVICE
 
 # echo "Restarting jails and running setup scripts..."
 # service jail start galatea reverse-proxy
 
-jexec -u root galatea       /SERVICE/setup.sh
-jexec -u root reverse-proxy /SERVICE/setup.sh
-jexec -u root auth-daemon   /SERVICE/setup.sh
+jexec -u root galatea          /SERVICE/setup.sh
+jexec -u root reverse-proxy    /SERVICE/setup.sh
+jexec -u root auth-daemon      /SERVICE/setup.sh
 jexec -u root authentication   /SERVICE/setup.sh
 
 echo "Production is ready!"
