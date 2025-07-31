6d8ea2087afdeae88eef16a1f10fd1ff65ded749
miyuki <miyuki@ane.jp.net>
2025-07-31 23:59:44 +0900

    Git Subsystem init project

---
+++ GitSystem/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="font-mplus2 min-h-screen bg-gray-900 text-white flex flex-col">
+    <div
+        class="flex flex-row font-kumbh px-4 py-2 bg-blue-400 text-center md:text-start"
+    >
+        ANE projecta
+    </div>
+    <div class="p-8 text-4xl">this shit is not done</div>
+</div>
