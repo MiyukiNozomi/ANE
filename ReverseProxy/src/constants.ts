66e3503d0ae074bed62be4d480e7473c2cce9803
miyuki <miyuki@ane.jp.net>
2025-08-26 13:34:46 +0900

    check to avoid skill issue

--- ReverseProxy/src/constants.ts
+++ ReverseProxy/src/constants.ts
@@ -1,5 +1,7 @@
 import { readFileSync } from "fs";
+import Sys from "./logging";
 
+export const IS_PREBUILD_SCRIPT = process.argv.includes("prebuild");
 export const IS_DEV_MODE = process.argv.includes("--dev");
 
 export const DATABASE_FILE = IS_DEV_MODE
@@ -8,9 +10,16 @@ export const DATABASE_FILE = IS_DEV_MODE
 
 export const DOMAIN_NAME = "ane.jp.net";
 
-export const SSLConfig = IS_DEV_MODE
-  ? null
-  : {
-      cert: readFileSync("./ssl/fullchain.pem"),
-      key: readFileSync("./ssl/privkey.pem"),
-    };
+if (IS_PREBUILD_SCRIPT) {
+  Sys.println(
+    "⚠️ You are running the prebuild script! if this is a false alarm, things will go really bad! ⚠️"
+  );
+}
+
+export const SSLConfig =
+  IS_DEV_MODE || IS_PREBUILD_SCRIPT
+    ? null
+    : {
+        cert: readFileSync("./ssl/fullchain.pem"),
+        key: readFileSync("./ssl/privkey.pem"),
+      };
