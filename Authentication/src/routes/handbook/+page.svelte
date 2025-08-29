8bd98cbbb082caa71c0e063ada63ca5b10a8360c
miyuki <miyuki@ane.jp.net>
2025-08-29 22:37:15 +0900

    Better organize the handbook

--- Authentication/src/routes/handbook/+page.svelte
+++ Authentication/src/routes/handbook/+page.svelte
@@ -295,16 +295,13 @@ It can also optionally include a URL to redirect the user back to if needed (if 
     </h2>
     <p>
       A recent addition to ANE is the Profile Picture support for accounts,
-      these are handled by this microservice on the following path:
+      they're stored in the <a
+        href="https://developers.google.com/speed/webp"
+        class="text-red-500 underline">WebP &UpperRightArrow;</a
+      > format. They're are handled by this microservice on the following path:
     </p>
     <div class="p-4 py-2 bg-slate-900 text-center rounded-lg">
       <p>https://auth.ane.jp.net/home/u/[username]/picture</p>
     </div>
-    <p>
-      Please note ALL profile pictures are in the <a
-        href="https://developers.google.com/speed/webp"
-        >WebP &UpperRightArrow;</a
-      > format.
-    </p>
   </div>
 </div>
